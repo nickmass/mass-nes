f2d9c02ecca43972b41db82a9dcf451fe5c451b9
Nick Massey <nickmass@nickmass.com>
2016-08-29 09:30:48 -0600

    Include missed file

--- src/nes/memory.rs
+++ src/nes/memory.rs
@@ -1,4 +1,5 @@
-use nes::system::SystemState;
+use nes::system::{System, SystemState};
+use nes::cartridge::Cartridge;
 use nes::bus::BusKind;
 
 #[derive(Copy, Clone)]
@@ -60,5 +61,135 @@ impl MemoryBlock {
 
     pub fn write(&self, bus: BusKind, state: &mut SystemState, addr: u16, val: u8) {
         state.mem.write(self.page, addr, val);
+    }
+}
+
+pub enum MemKind {
+    Prg,
+    Chr,
+}
+
+pub struct Banks {
+    data: Vec<Page>,
+    kind: MemKind,
+}
+
+impl Banks {
+    pub fn load(cart: &Cartridge, kind: MemKind) -> Banks {
+        let data: &[u8] = match kind {
+            MemKind::Prg => &*cart.prg_rom,
+            MemKind::Chr => &*cart.chr_rom,
+        };
+        let mut v = Vec::new();
+        let mut x = 0;
+        while x + 0x400 <= data.len() {
+            let start = x;
+            x += 0x400;
+            let end = x;
+            v.push(Page { start: start, end: end });
+        }
+        Banks {
+            data: v,
+            kind: kind,
+        }
+    }
+
+    pub fn read(&self, system: &System, bank: usize, addr: u16) -> u8 {
+        let bank = self.data[bank];
+        match self.kind {
+            MemKind::Prg =>
+                system.cartridge.prg_rom[bank.start + addr as usize],
+            MemKind::Chr => 
+                system.cartridge.chr_rom[bank.start + addr as usize],
+        }
+    }
+}
+
+#[derive(Copy, Clone)]
+enum Mapped {
+    Page(usize),
+    Bank(usize),
+}
+
+pub enum BankKind {
+    Ram,
+    Rom,
+}
+
+pub struct MappedMemory {
+    banks: Banks,
+    pages: Vec<Page>,
+    base_addr: u16,
+    mapping: Vec<Mapped>,
+}
+
+impl MappedMemory {
+    pub fn new(state: &mut SystemState, cart: &Cartridge, base_addr: u16,
+               ram_kb: u32, size_kb: u32, kind: MemKind) -> MappedMemory {
+        let mut pages = Vec::new();
+        let mut mapping = Vec::new();
+
+        for x in 0..ram_kb {
+            pages.push(state.mem.alloc_kb(1));
+        }
+        for x in 0..size_kb {
+            mapping.push(Mapped::Bank(x as usize));
+        }
+        MappedMemory {
+            banks: Banks::load(cart, kind),
+            pages: pages,
+            base_addr: base_addr,
+            mapping: mapping,
+        }
+    }
+
+    pub fn map(&mut self, addr: u16, kb: u32, bank: usize, bank_kind: BankKind) {
+        if addr & 0x3ff != 0 { panic!("Must map in 1kb chunks"); }
+        let offset = (addr - self.base_addr) / 0x400;
+        let bank_start = (bank * kb as usize) ;//% self.mapping.len();
+
+        match bank_kind {
+            BankKind::Rom => {
+                for b in 0..kb as usize {
+                    self.mapping[offset as usize + b] = Mapped::Bank(b + bank_start);
+                }
+            },
+            BankKind::Ram => {
+                for b in 0..kb as usize{
+                    self.mapping[offset as usize + b] = Mapped::Page(b + bank_start);
+                }
+            }
+        }
+    }
+
+    fn get_mapping(&self, addr: u16) -> Mapped {
+        let offset = (addr - self.base_addr) / 0x400;
+        self.mapping[offset as usize]
+    }
+
+    pub fn read(&self, system: &System, state: &SystemState, addr: u16) -> u8 {
+        let mapping = self.get_mapping(addr);
+
+        match mapping {
+            Mapped::Bank(b) => {
+                self.banks.read(system, b, addr & 0x3ff)
+            },
+            Mapped::Page(p) => {
+                let page = self.pages[p];
+                state.mem.read(page, addr & 0x3ff)
+            }
+        }
+    }
+
+    pub fn write(&self, system: &System, state: &mut SystemState, addr: u16, val: u8) {
+        let mapping = self.get_mapping(addr);
+
+        match mapping {
+            Mapped::Bank(b) => {},
+            Mapped::Page(p) => {
+                let page = self.pages[p];
+                state.mem.write(page, addr & 0x3ff, val);
+            }
+        }
     }
 }
