1ef9616a259dce22e2c5e880165e0c1068a3571c
Nick Massey <nickmass@nickmass.com>
2016-10-02 16:30:02 -0600

    Directly pass screen array from emscripten

--- src/web/src/main.rs
+++ src/web/src/main.rs
@@ -1,3 +1,35 @@
+#![feature(libc)]
+extern crate libc;
+extern "C" {
+    fn emscripten_asm_const_int(script: *const libc::c_char, ...) -> libc::c_int;
+}
+
+macro_rules! c_str {
+    ($s:expr) => {
+        concat!($s, "\0").as_ptr() as *const _
+    }
+}
+
+macro_rules! js {
+    (($($name:ident = $expr:expr),*) {$($tt:tt)*}) => {
+        unsafe { emscripten_asm_const_int(c_str!(concat!(
+            $(
+                "var ", stringify!($name), " = $0; ",
+            )*
+             stringify!($($tt)*))), $($expr),*) }
+    };
+}
+
+macro_rules! js_raw {
+    (($($name:ident = $expr:expr),*) {$code:expr}) => {
+        unsafe { emscripten_asm_const_int(c_str!(concat!(
+            $(
+                "var ", stringify!($name), " = $0; ",
+            )*
+             $code)), $($expr),*) }
+    };
+}
+
 extern crate mass_nes;
 
 use mass_nes::nes::{UserInput, Controller, Machine, Cartridge, Region};
@@ -9,22 +41,14 @@ fn main() {
     let cart = Cartridge::load(&mut (rom as &[u8])).unwrap();
 
     let mut machine = Machine::new(region, cart, |screen| {
-        let mut string = screen.iter().map(|i| {
+        let screen: Vec<u32> = screen.iter().map(|i| {
             let mut color: u32 = 0;
             color |= pal[(i*3) as usize] as u32;
             color |= (pal[((i*3) + 1) as usize] as u32) << 8;
             color |= (pal[((i*3) + 2) as usize] as u32) << 16;
             color
-        }).fold(String::new(), |mut acc, c| {
-            if acc.len() == 0 {
-                acc.push_str(&format!("[{}", c));
-            } else {
-                acc.push_str(&format!(",{}", c));
-            }
-            acc
-        });
-        string.push(']');
-        println!("{}", string);
+        }).collect();
+        js_raw!{(js_screen = &*screen){r#"var screen = Module.HEAPU32.subarray(js_screen >> 2, (js_screen >> 2)+(256*240));postMessage(screen);"#}};
     }, |samples| {
     }, || {
         let mut r = Vec::new();
--- src/web/src/worker.js
+++
@@ -1,9 +0,0 @@
-function fake_log(arg) {
-	if (arg[0] != '[') {return;}
-	var screen = eval(arg);
-	postMessage(screen);
-}
-
-console.log = fake_log;
-
-importScripts('main.js');
