5ce5a7f32b3a3ce1d7cee5637e4cddc35c264c45
Nick Massey <nickmass@nickmass.com>
2025-01-13 15:54:36 -0700

    Move to much better maintained wasm-tracing crate

--- crates/web/src/lib.rs
+++ crates/web/src/lib.rs
@@ -19,11 +19,7 @@ use app::UserEvent;
 #[wasm_bindgen(start)]
 pub fn main() {
     console_error_panic_hook::set_once();
-    tracing_wasm::set_as_global_default_with_config(
-        tracing_wasm::WASMLayerConfigBuilder::new()
-            .set_max_level(tracing::Level::DEBUG)
-            .build(),
-    )
+    wasm_tracing::set_as_global_default();
 }
 
 #[wasm_bindgen]
